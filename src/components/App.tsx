2cd6d421369e29eec3970eea34a0649e3aa88f7f
Florian Marending <florian@marending.dev>
2021-08-23 20:17:10 +0200

    Add logout button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { user_logout } from '../api/user_api';
 import { useNoteList } from '../context/noteListReducer';
 import { NoteListEntry } from '../types';
 
 const App = (): React.ReactElement => {
   const notes = useNoteList();
+
+  const logout = () => {
+    user_logout();
+  };
 
   return (
     <>
@@ -13,6 +18,12 @@ const App = (): React.ReactElement => {
           <li>{note.metainfo}</li>
         ))}
       </ul>
+      <button
+        onClick={() => logout()}
+        className="rounded-sm bg-gray-200 p-2 m-2 hover:bg-gray-400"
+      >
+        Logout
+      </button>
     </>
   );
 };
