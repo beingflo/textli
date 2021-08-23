a1f349ed9a1e8b2910bc78b12a85887d25b977f5
Florian Marending <florian@marending.dev>
2021-08-23 17:17:24 +0200

    Change color of spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -5,8 +5,8 @@ export const Spinner = (): React.ReactElement => {
   return (
     <div className="flex h-48 md:h-screen">
       <div className="spinner m-auto">
-        <div className="double-bounce1"></div>
-        <div className="double-bounce2"></div>
+        <div className="double-bounce1 bg-yellow-600"></div>
+        <div className="double-bounce2 bg-yellow-600"></div>
       </div>
     </div>
   );
