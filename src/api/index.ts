e161589a2d1692b50bdf7c56e10fc4ae92ab3dd5
Florian Marending <florian@marending.dev>
2021-08-23 22:31:53 +0200

    Add proper types

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,12 @@
 import { toast } from 'react-toastify';
 
-export const mapError = (response: any): any => {
+export const mapError = (response: Response): any => {
   if (!response.ok) {
     throw Error(response.statusText);
   }
   return response;
 };
 
-export const handleException = (error: any): void => {
+export const handleException = (error: Error): void => {
   toast.error(error.toString());
 };
