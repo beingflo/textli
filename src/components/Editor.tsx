0528d6d232144fff5f96e76af6887ed8ff72f723
Florian Marending <florian@marending.dev>
2021-08-24 16:48:24 +0200

    Add extensions

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -3,13 +3,26 @@ import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Highlight from '@tiptap/extension-highlight';
 import Typography from '@tiptap/extension-typography';
+import Placeholder from '@tiptap/extension-placeholder';
+import FontFamily from '@tiptap/extension-font-family';
+import TextStyle from '@tiptap/extension-text-style';
+import './editorStyles.css';
 
 export const Editor = (): React.ReactElement => {
   const editor = useEditor({
-    extensions: [StarterKit, Highlight, Typography],
-    content: '<p>Write something nice...</p>',
+    extensions: [
+      StarterKit,
+      Highlight,
+      Typography,
+      Placeholder.configure({
+        placeholder: 'Write something nice ...',
+      }),
+      FontFamily,
+      TextStyle,
+    ],
     autofocus: 'end',
     editable: true,
+    content: null,
     editorProps: {
       attributes: {
         class: 'prose p-2 focus:outline-none h-screen',
