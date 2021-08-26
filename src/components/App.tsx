289e1f2431527ab0a1abbbceb8ad31603aab5bc2
Florian Marending <florian@marending.dev>
2021-08-26 21:20:29 +0200

    Remove shadow from sidebar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -19,7 +19,7 @@ const App = (): React.ReactElement => {
             leave="transition ease-in-out duration-300 transform"
             leaveFrom="translate-x-0"
             leaveTo="-translate-x-full"
-            className="absolute bg-gray-100 z-20 shadow-2xl h-full w-80 sm:w-96"
+            className="absolute bg-gray-100 z-20 h-full w-80 sm:w-96"
           >
             <Sidebar setHide={() => setShowSidebar(false)} />
           </Transition.Child>
