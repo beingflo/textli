50a9e9e1641abbadac388095ec9eda4a4dd06173
Florian Marending <florian@marending.dev>
2021-08-26 15:45:53 +0200

    Close sidebar on click outside

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,17 +10,32 @@ const App = (): React.ReactElement => {
   return (
     <div className="h-auto flex justify-between w-full relative">
       <div className="w-12">
-        <Transition
-          show={showSidebar}
-          enter="transition ease-in-out duration-300 transform"
-          enterFrom="-translate-x-full"
-          enterTo="translate-x-0"
-          leave="transition ease-in-out duration-300 transform"
-          leaveFrom="translate-x-0"
-          leaveTo="-translate-x-full"
-          className="absolute bg-gray-100 z-10 shadow-2xl h-full w-80 sm:w-96"
-        >
-          <Sidebar setHide={() => setShowSidebar(false)} />
+        <Transition show={showSidebar}>
+          <Transition.Child
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom="-translate-x-full"
+            enterTo="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="-translate-x-full"
+            className="absolute bg-gray-100 z-20 shadow-2xl h-full w-80 sm:w-96"
+          >
+            <Sidebar setHide={() => setShowSidebar(false)} />
+          </Transition.Child>
+          <Transition.Child
+            enter="transition-opacity ease-linear duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-linear duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            className="absolute h-full w-full z-10"
+          >
+            <div
+              onClick={() => setShowSidebar(false)}
+              className="h-screen w-full"
+            ></div>
+          </Transition.Child>
         </Transition>
         <Transition
           show={!showSidebar}
