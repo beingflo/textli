a1cb218f61c3a9dc7605dd168ae18637c13c3fa9
Florian Marending <florian@marending.dev>
2021-08-29 21:04:08 +0200

    Add close icon

--- src/icons.tsx
+++ src/icons.tsx
@@ -418,3 +418,26 @@ export const ClearIcon = ({
     </svg>
   );
 };
+
+export const CloseIcon = ({
+  className,
+}: {
+  className?: string;
+}): React.ReactElement => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  );
+};
