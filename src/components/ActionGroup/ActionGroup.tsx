85c16a24f9bf4f08d288a36edc44f1bf6cb0e586
Florian Marending <florian@marending.dev>
2021-08-29 21:19:04 +0200

    Add transition to modal

--- src/components/ActionGroup/ActionGroup.tsx
+++ src/components/ActionGroup/ActionGroup.tsx
@@ -55,27 +55,45 @@ export const ActionGroup = (): React.ReactElement => {
           </Popover.Panel>
         </Transition>
       </Popover>
-      <Dialog
-        open={showSettings}
-        onClose={() => setShowSettings(false)}
-        className="fixed z-10 inset-0 overflow-y-auto"
-      >
-        <div className="flex items-center justify-center min-h-screen">
-          <Dialog.Overlay className="fixed inset-0 bg-gray-200 opacity-40" />
+      <Transition show={showSettings} as={React.Fragment}>
+        <Dialog
+          onClose={() => setShowSettings(false)}
+          className="fixed z-10 inset-0 overflow-y-auto"
+        >
+          <div className="flex items-center justify-center min-h-screen">
+            <Transition.Child
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-200 opacity-40" />
+            </Transition.Child>
+            <Transition.Child
+              enter="transition-opacity ease-linear duration-150"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-75"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="relative p-6 bg-white rounded max-w-lg mx-auto">
+                <div className="flex flex-row justify-between min-w-md">
+                  <Dialog.Title className="text-2xl font-bold highlight inline">
+                    Settings
+                  </Dialog.Title>
 
-          <div className="relative p-6 bg-white rounded max-w-lg mx-auto">
-            <div className="flex flex-row justify-between min-w-md">
-              <Dialog.Title className="text-2xl font-bold highlight inline">
-                Settings
-              </Dialog.Title>
-
-              <button onClick={() => setShowSettings(false)}>
-                <CloseIcon className="h-6 w-6" />
-              </button>
-            </div>
+                  <button onClick={() => setShowSettings(false)}>
+                    <CloseIcon className="h-6 w-6" />
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
           </div>
-        </div>
-      </Dialog>
+        </Dialog>
+      </Transition>
     </div>
   );
 };
