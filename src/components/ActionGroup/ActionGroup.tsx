ffaaa3a9a43f2a614271d470f15691d9787fbcb7
Florian Marending <florian@marending.dev>
2021-08-29 21:04:16 +0200

    Add modal for settings

--- src/components/ActionGroup/ActionGroup.tsx
+++ src/components/ActionGroup/ActionGroup.tsx
@@ -1,15 +1,23 @@
-import { Popover, Transition } from '@headlessui/react';
+import { Dialog, Popover, Transition } from '@headlessui/react';
 import React from 'react';
 import { get_notes } from '../../api/note_api';
 import { user_logout } from '../../api/user_api';
 import { useAppDispatch } from '../../context';
-import { LinkIcon, LogoutIcon, MoreIcon, SettingsIcon } from '../../icons';
+import {
+  CloseIcon,
+  LinkIcon,
+  LogoutIcon,
+  MoreIcon,
+  SettingsIcon,
+} from '../../icons';
 import { SaveAction } from './SaveAction';
 import { DeleteAction } from './DeleteAction';
 import { NewAction } from './NewAction';
+import '../../style.css';
 
 export const ActionGroup = (): React.ReactElement => {
   const dispatch = useAppDispatch();
+  const [showSettings, setShowSettings] = React.useState(false);
 
   const handleLogout = React.useCallback(() => {
     user_logout(() => get_notes(dispatch));
@@ -37,7 +45,7 @@ export const ActionGroup = (): React.ReactElement => {
               <button>
                 <LinkIcon className="text-gray-700 hover:-translate-x-0.5 transform transition active:scale-90" />
               </button>
-              <button>
+              <button onClick={() => setShowSettings(true)}>
                 <SettingsIcon className="text-gray-700 hover:-translate-x-0.5 transform transition active:scale-90" />
               </button>
               <button onClick={handleLogout}>
@@ -47,6 +55,27 @@ export const ActionGroup = (): React.ReactElement => {
           </Popover.Panel>
         </Transition>
       </Popover>
+      <Dialog
+        open={showSettings}
+        onClose={() => setShowSettings(false)}
+        className="fixed z-10 inset-0 overflow-y-auto"
+      >
+        <div className="flex items-center justify-center min-h-screen">
+          <Dialog.Overlay className="fixed inset-0 bg-gray-200 opacity-40" />
+
+          <div className="relative p-6 bg-white rounded max-w-lg mx-auto">
+            <div className="flex flex-row justify-between min-w-md">
+              <Dialog.Title className="text-2xl font-bold highlight inline">
+                Settings
+              </Dialog.Title>
+
+              <button onClick={() => setShowSettings(false)}>
+                <CloseIcon className="h-6 w-6" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </Dialog>
     </div>
   );
 };
