27f4e4a9c85293e12e70ab74bfea10c1af7e7868
Florian Marending <florian@marending.dev>
2021-09-07 19:23:52 +0200

    Move metadata

--- src/components/util.ts
+++ src/components/util.ts
@@ -1,10 +1,5 @@
 import React from 'react';
 
-export type Metadata = {
-  title: string;
-  tags: string;
-};
-
 export const getMetadata = (content: string | undefined): string => {
   const title = new DOMParser().parseFromString(content ?? '', 'text/html');
   const trimmedTitle = title.body.firstElementChild?.textContent ?? '';
