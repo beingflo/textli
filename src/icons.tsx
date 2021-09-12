7aa7bcb5c9455c2a2371aff91e0659704e1b1aa8
Florian Marending <florian@marending.dev>
2021-09-12 12:51:34 +0200

    Add selector icon

--- src/icons.tsx
+++ src/icons.tsx
@@ -490,3 +490,26 @@ export const CopyIcon = ({
     </svg>
   );
 };
+
+export const SelectorIcon = ({
+  className,
+}: {
+  className?: string;
+}): React.ReactElement => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M8 9l4-4 4 4m0 6l-4 4-4-4"
+      />
+    </svg>
+  );
+};
