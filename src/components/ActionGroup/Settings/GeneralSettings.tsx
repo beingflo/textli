92ca1f6670336cb5db8b2a23ae85e6fc33a53afc
Florian Marending <florian@marending.dev>
2021-11-21 12:02:02 +0100

    Make back button react to hover

--- src/components/ActionGroup/Settings/GeneralSettings.tsx
+++ src/components/ActionGroup/Settings/GeneralSettings.tsx
@@ -117,7 +117,7 @@ const SessionConfirm = ({ setSessionConfirm }: any): React.ReactElement => {
     <>
       <button
         onClick={() => setSessionConfirm(false)}
-        className="mb-4 text-gray-600"
+        className="mb-4 text-gray-600 hover:-translate-x-0.5 transform transition active:translate-x-0"
       >
         <div className="flex flex-row gap-1">
           <ArrowLeftIcon className="w-6 h-6" />
@@ -189,7 +189,7 @@ const PasswordConfirm = ({ setPasswordConfirm }: any): React.ReactElement => {
     <div>
       <button
         onClick={() => setPasswordConfirm(false)}
-        className="mb-4 text-gray-600"
+        className="mb-4 text-gray-600 hover:-translate-x-0.5 transform transition active:translate-x-0"
       >
         <div className="flex flex-row gap-1">
           <ArrowLeftIcon className="w-6 h-6" />
@@ -276,7 +276,7 @@ const DeleteConfirm = ({ setDeleteConfirm }: any): React.ReactElement => {
     <>
       <button
         onClick={() => setDeleteConfirm(false)}
-        className="mb-4 text-gray-600"
+        className="mb-4 text-gray-600 hover:-translate-x-0.5 transform transition active:translate-x-0"
       >
         <div className="flex flex-row gap-1">
           <ArrowLeftIcon className="w-6 h-6" />
