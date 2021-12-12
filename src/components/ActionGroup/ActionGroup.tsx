bb40c46a6d743e175e6447eba9dc82dd6b35ffec
Florian Marending <florian@marending.dev>
2021-12-12 19:55:12 +0100

    Apply outline styling on button

--- src/components/ActionGroup/ActionGroup.tsx
+++ src/components/ActionGroup/ActionGroup.tsx
@@ -70,9 +70,9 @@ export const ActionGroup = (): React.ReactElement => {
         </button>
       </div>
       <Popover className="relative">
-        <Popover.Button>
+        <Popover.Button className="focus:outline-none outline-none">
           <div>
-            <MoreIcon className="h-7 w-7 sm:h-6 sm:w-6 outline-none focus:outline-none text-gray-700 hover:-translate-x-0.5 transform transition active:scale-90" />
+            <MoreIcon className="h-7 w-7 sm:h-6 sm:w-6 text-gray-700 hover:-translate-x-0.5 transform transition active:scale-90" />
           </div>
         </Popover.Button>
         <Transition
